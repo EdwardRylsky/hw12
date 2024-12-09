447523b090a913ac6f4de0d94d06acab6e5e20c0
Edward_Rylsky <edwardrylsky@gmail.com>
2024-12-09 22:30:57 +0500

    hw12 feat frogSteps

--- src/main/java/org/example/HomeWork.java
+++ src/main/java/org/example/HomeWork.java
@@ -5,6 +5,8 @@ import lombok.SneakyThrows;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class HomeWork {
 
@@ -14,8 +16,33 @@ public class HomeWork {
      */
     @SneakyThrows
     public void frogSteps(InputStream in, OutputStream out) {
+        Objects.requireNonNull(in, "Parameter 'in' can't be null");
+        Objects.requireNonNull(out, "Parameter 'out' can't be null");
+
+        try (Scanner scanner = new Scanner(in)) {
+            int N = new Scanner(scanner.nextLine()).nextInt();
+
+            for (int i = 0; i < N; i++) {
+                String line = scanner.nextLine();
+                int result = countL(line);
+                out.write(Integer.toString(result).getBytes());
+                out.write("\r\n".getBytes());
+            }
+            out.flush();
+
+        }
     }
 
+    private int countL(String line) {
+        int maxLSubstringLength = 0;
 
+        String[] split = line.split("R");
+        for (String s : split) {
+            if (s.length() > maxLSubstringLength) {
+                maxLSubstringLength = s.length();
+            }
+        }
 
+        return maxLSubstringLength + 1;
+    }
 }
